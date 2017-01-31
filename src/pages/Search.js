43bdf79990efc47810b4bc8d07157ca19789b5a4
Kjartan Angamaiton <ofurmadur@icloud.com>
2017-01-31 08:59:10 -0600

    test

---
+++ src/pages/Search.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Search extends Component {
+  render() {
+    return (
+      <div>
+        Hello
+      </div>
+    );
+  }
+}
+
+export default Search;
