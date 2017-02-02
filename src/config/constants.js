e520302e21016b92816ea4c3319e4f8c8c15f236
Kjartan Angamaiton <ofurmadur@icloud.com>
2017-02-02 14:01:15 -0600

    user successfully added

--- src/config/constants.js
+++ src/config/constants.js
@@ -4,6 +4,8 @@ const config = {
   apiKey: 'AIzaSyCcfqngXjxwKNjuj2stWhLwmH6D2IMd9ac',
   authDomain: 'linguaphyle-react.firebaseapp.com',
   databaseURL: 'https://linguaphyle-react.firebaseio.com',
+  storageBucket: 'linguaphyle-react.appspot.com',
+  messagingSenderId: '1071573957158',
 };
 
 export const ref = firebase.database().ref();
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,22 +1,23 @@
 import React, { Component } from 'react';
-import { auth } from '../helpers/auth';
+import firebase from 'firebase';
 
 export default class Register extends Component {
   handleSubmit(e) {
     e.preventDefault();
-    auth(this.email.value, this.pw.value);
+    const firebaseAuth = firebase.auth();
+    firebaseAuth.createUserWithEmailAndPassword(this.email.value, this.pw.value);
   }
   render() {
     return (
       <div className="col-6">
         <h1>Register</h1>
-        <form onSubmit={this.handleSubmit}>
+        <form onSubmit={e => this.handleSubmit(e)}>
           <div className="form-group">
             <label htmlFor="registerEmail">Email</label>
             <input
               className="form-control"
               id="registerEmail"
-              ref={(email) => this.email = email} placeholder="Email"
+              ref={email => (this.email = email)} placeholder="Email"
             />
           </div>
           <div className="form-group">
@@ -25,7 +26,7 @@ export default class Register extends Component {
               type="password"
               className="form-control"
               id="registerPass"
-              placeholder="Password" ref={(pw) => this.pw = pw}
+              placeholder="Password" ref={pw => (this.pw = pw)}
             />
           </div>
           <button type="submit" className="btn btn-primary">Register</button>
